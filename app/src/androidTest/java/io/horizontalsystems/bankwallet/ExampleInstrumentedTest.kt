ac2cef609a40c0852cbbcf1052e3a9c0b76d7b8e
HashEngineering <hashengineeringsolutions@gmail.com>
2020-04-18 10:05:48 -0700

    Update testnet icons

--- app/src/androidTest/java/io/horizontalsystems/bankwallet/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/io/horizontalsystems/bankwallet/ExampleInstrumentedTest.kt
@@ -2,6 +2,8 @@ package io.horizontalsystems.bankwallet
 
 import androidx.test.InstrumentationRegistry
 import androidx.test.runner.AndroidJUnit4
+import com.google.common.base.Stopwatch
+import io.horizontalsystems.groestlcoinkit.GroestlHasher
 import org.junit.Assert.assertEquals
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -17,6 +19,28 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getTargetContext()
-        assertEquals("io.horizontalsystems.bankwallet", appContext.packageName)
+        //assertEquals("io.horizontalsystems.bankwallet", appContext.packageName)
+    }
+
+    @Test
+    fun determineSpeed() {
+        val bytes = ByteArray(32)
+
+        val watch = Stopwatch.createStarted()
+
+        for(i in 0..10000) {
+            GroestlHasher().groestldNative(bytes, 0, bytes.size)
+        }
+        watch.stop()
+        println("stopwatch, native: $watch")
+
+        watch.reset()
+        watch.start()
+        for(i in 0..10000) {
+            GroestlHasher().groestldKotlin(bytes)
+        }
+        watch.stop()
+                println("stopwatch, native: $watch")
+
     }
 }
